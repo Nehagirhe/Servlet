70b6b8e42bce99ff95429bf2b107089a51ec6794
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-12-24 18:56:52 +0530

    Add files via upload

---
+++ day_4/build/classes/src/Servlet1.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		ServletConfig c=getServletConfig();
+		String hrName=c.getInitParameter("Geeta");
+		
+		out.println("A paragraph is a unit of writing that consists of "
+				+hrName+ "one or more sentences "+hrName+" that relate to the same "
+				+ "topic. "+hrName+" Paragraphs are essential "+hrName+" for organizing"
+				+ " long "
+				+ "pieces of writing such as novels, papers, or even emails.");
+		
+		
+	}
+
+}
